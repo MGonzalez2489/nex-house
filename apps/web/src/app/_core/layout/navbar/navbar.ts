f4361cb6e31926e921c135751604402dffc69103
ManuelGonzalez <manuel_agh@hotmail.com>
2026-03-12 18:09:46 -0600

    feat(ui): user visible identity

--- apps/web/src/app/_core/layout/navbar/navbar.ts
+++ apps/web/src/app/_core/layout/navbar/navbar.ts
@@ -1,16 +1,43 @@
-import { Component, input, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  output,
+} from '@angular/core';
 import { UserModel } from '@nex-house/models';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule, MenuModule],
+  imports: [ButtonModule, MenuModule, AvatarModule, AvatarGroupModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navbar {
   user = input<UserModel>();
   toggleSidebar = output();
   logout = output();
+
+  avatarLabel = computed(() => {
+    const cUser = this.user();
+    if (!cUser) return;
+
+    let r = '';
+
+    if (cUser.firstName) {
+      r += cUser.firstName[0];
+    }
+    if (cUser.lastName) {
+      r += cUser.lastName[0];
+    }
+
+    return r;
+  });
 }
--- apps/web/src/app/_core/layout/sidebar/sidebar.ts
+++ apps/web/src/app/_core/layout/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserModel } from '@nex-house/models';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,6 +9,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  user = input<UserModel>();
   menuItems = signal([
     { label: 'Dashboard', path: '/dashboard', icon: 'pi pi-chart-bar' },
     { label: 'Neighborhoods', path: '/neighborhoods', icon: 'pi pi-map' },
